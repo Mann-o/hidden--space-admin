4003090c899fa55d5a086629d9e019e6927cd846
Manno <me@liam-potter.co.uk>
2019-10-18 12:10:45 +0100

    post images

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,32 +1,84 @@
 <template lang="pug">
   .page-posts-slug
     Breadcrumbs(:crumbs="crumbs")
-    pre(v-if="post != null") {{ post }}
+
+    BCardGroup(deck)
+      BCard(header="Information" header-tag="h6")
+        BForm(@submit.prevent="save")
+          BFormGroup(label="Slug" label-for="slug" label-cols="2")
+            BFormInput(id="slug" v-model="post.slug")
+          BFormGroup(label="Author" label-for="author" label-cols="2")
+            BFormSelect(id="author" :options="postAuthors" v-model="post.authorId")
+          BFormGroup(label="Title" label-for="title" label-cols="2")
+            BFormInput(id="title" v-model="post.title")
+          BFormGroup(label="Content" label-for="content" label-cols="2")
+            quill(id="content" v-model="post.content" output="html" :config="quillConfig")
+          BFormGroup
+            SpinnerButton(type="submit" :disabled="isSaving" :loading="isSaving" label="Save Changes")
+
+      BCard(header="Media" header-tag="h6")
+        BButton.mr-2(v-b-modal.browse-images-modal variant="success") Add Images
+        BButton(variant="danger" v-if="hasImagesSelected" @click="removeSelectedImages") Remove ({{ selectedImagesCount }})
+        Gallery(v-if="images.length" :items="images" @toggle="toggle" padded)
+        BAlert.gallery__empty(v-else variant="warning" show) No images have been added yet!
+
+    ModalGallery(
+      title="Select Images"
+      :exclude="images"
+      @ok="addSelectedImages"
+    )
 </template>
 
 <script>
+import update from '@/mixins/crud/update'
+
 export default {
   name: 'PagePostsSlug',
 
-  transition: 'fade',
-
-  components: {
-    Breadcrumbs: () => import('@/components/layout/Breadcrumbs'),
-  },
+  mixins: [update('post', 'slug', 'title')],
 
   data: () => ({
-    post: null,
+    authors: [],
+    quillConfig: {
+      placeholder: 'Enter some text for this Post',
+      modules: {
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ script: 'sub' }, { script: 'super' }],
+          [{ indent: '-1' }, { indent: '+1' }],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }],
+          ['clean'],
+        ],
+      },
+      theme: 'bubble',
+    },
   }),
 
   computed: {
-    crumbs () {
-      return [{ text: 'Posts', to: '/posts' }, { text: this.post == null ? '' : this.post.title, active: true }]
+    postAuthors () {
+      return this.authors == null ? [] : this.authors.map(({ id, username }) => ({
+        value: id,
+        text: username,
+      }))
     },
   },
 
-  async asyncData ({ app: { $axios }, params }) {
-    const { data: post } = await $axios.get(`/api/posts/${params.slug}`)
-    return { post }
+  async mounted () {
+    const { data: authors } = await this.$api.users.index()
+    this.authors = authors
   },
 }
 </script>
+
+<style lang="stylus">
+@import '~assets/styles/core/mixins/bem'
+
+.gallery
+  .alert
+    margin-bottom: 0
+
+  +has(empty)
+    margin-top: 1.4rem
+</style>
